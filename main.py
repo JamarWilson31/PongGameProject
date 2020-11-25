4926c3620961c8883b74c5ba90ddac51308fd779
Jamar Wilson <jamarjwilson@gmail.com>
2020-11-25 19:45:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+#Pong In python 3
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by @Jaydubbs")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+
+
+#Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color('white')
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+#Paddle B
+
+#Ball
+
+
+
+
+#Main game loop
+while True:
+      wn.update()
